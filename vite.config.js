6d413f5a3b2cd2fdb9535680b0cc753388eadfec
aylin <4ylingunes@gmail.com>
2026-03-10 13:03:20 +0100

    Fix multi-page build for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath } from 'node:url'
 
 export default defineConfig({
   root: 'src',
   base: '/goit-js-hw-10/',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./src/index.html', import.meta.url)),
+        timer: fileURLToPath(new URL('./src/01-timer.html', import.meta.url)),
+        snackbar: fileURLToPath(new URL('./src/02-snackbar.html', import.meta.url))
+      }
+    }
   }
 })
